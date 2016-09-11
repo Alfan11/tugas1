efc14e0a64e9311f9061485f8ab0b628b29af7b3
USER-ASUS <Alfancahyo11@gmail.com>
2016-09-11 16:54:22 +0700

    keenam

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl5003/tugas1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl5003/tugas1/MainActivity.java
@@ -5,19 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-
 public class MainActivity extends AppCompatActivity {
     EditText etNama;
     EditText etTahun;
     Spinner spinner;
     Button bOk;
-    TextView tvHasil;
+    TextView tvHasil, tvHasil2;
     RadioButton rbL, rbP;
     CheckBox cbT, cbM, cbL;
 
@@ -37,16 +38,19 @@ public class MainActivity extends AppCompatActivity {
         cbT = (CheckBox) findViewById(R.id.checkBoxT);
         cbM = (CheckBox) findViewById(R.id.checkBoxM);
         cbL = (CheckBox) findViewById(R.id.checkBoxL);
+        final Animation animbOk = AnimationUtils.loadAnimation(this, R.anim.anim_rotate);
+
 
         tvHasil = (TextView) findViewById(R.id.textView4);
+        tvHasil2 = (TextView) findViewById(R.id.textView6);
+        Button btnbutton = (Button)findViewById(R.id.rotate);
 
 
         bOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                String nama = etNama.getText().toString();
-                String tahun = etTahun.getText().toString();
+                doProcess();
 
                 String asal = spinner.getSelectedItem().toString();
 
@@ -62,24 +66,28 @@ public class MainActivity extends AppCompatActivity {
                 if (cbT.isChecked()) hsl2 += cbT.getText() + "\n";
                 if (cbM.isChecked()) hsl2 += cbM.getText() + "\n";
                 if (cbL.isChecked()) hsl2 += cbL.getText() + "\n";
-                if (hsl2.length() == startlen) hsl2 += "Anda tidak punya kesukaan";
 
-                tvHasil.setText("Nama  :\n" + nama + "\n\nTahun Lahir : \n" + tahun + "\n\nJenis Kelamim : \n" + hsl1 + "\n\n Asal Kota : \n" + asal + "\n\n" + hsl2);
+                if (hsl2.length() == startlen) hsl2 += "Belum Pernah Memilih";
+                if (hsl2.length() == startlen) hsl2 += "Belum Pernah Memilih";
+                tvHasil2.setText("\n\nJenis Kelamin : \n" + hsl1 + "\n\n Asal Kota : \n" + asal + "\n\n" + hsl2);
+                view.startAnimation(animbOk);
+
             }
         });
+
 
         etTahun.setText("");
         etNama.setText("");
 
     }
 
-    private void doHasil()
-        {
+    private void doProcess()
+    {
         if(isValid())
         {
             String nama =  etNama.getText().toString();
-            int tahun = Integer.parseInt(etTahun.getText().toString());
-
+            String tahun = etTahun.getText().toString();
+            tvHasil.setText("Nama Depan :\n" + nama + "\n\nTahun Lahir : \n" + tahun);
 
         }
     }
@@ -90,7 +98,6 @@ public class MainActivity extends AppCompatActivity {
 
         String nama = etNama.getText().toString();
         String tahun = etTahun.getText().toString();
-
 
         if(nama.isEmpty())
         {
